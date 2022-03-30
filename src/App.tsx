4dc4f83f881aa201f356744d7d69840784746cdd
Rustam-Ulaevich <praporglamur@gmail.com>
2022-03-30 10:58:53 +0500

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import {Todolist} from './Todolist';
+
+
+
+function App() {
+
+
+return <div>
+    <Todolist />
+</div>
+}
+
+export default App;
---
+++ src/Todolist.tsx
@@ -0,0 +1,6 @@
+
+export function Todolist() {
+    return <div>
+
+    </div>
+}
