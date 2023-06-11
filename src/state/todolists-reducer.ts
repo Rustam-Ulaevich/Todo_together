1e8a3300a67fcfb8bb416af8369b9c8ab88698b1
Rustam-Ulaevich <praporglamur@gmail.com>
2023-06-11 23:58:27 +0500

    The 8th lesson has been completed

--- src/state/todolists-reducer.ts
+++ src/state/todolists-reducer.ts
@@ -1,24 +1,29 @@
 import {TodolistType} from "../App";
+// type RemoveTodolistType={
+//     type:'REMOVE-TODOLIST'
+//     id:string
+// }
+type RemoveTodolistType = ReturnType<typeof removeTodolistAC>
+type ActionType = RemoveTodolistType
 
-type ActionType = {
-    type: string
-    [key: string]: string
-}
 
 export const todolistsReducer = (state: Array<TodolistType>, action: ActionType) => {
     switch (action.type) {
         case 'REMOVE-TODOLIST':
+            return {
+                ...state, [action.id]: state.filter(el => {
+                    return el.id !== action.id
+                })
+            }
+
 
             return state
 
-        case 'ADD-TODOLIST':
-
-            return state
-
-        case 'RENAME-TODOLIST':
-
-            return state
-
-        defualt: throw new Error('This action type is undefined')
+            defualt: throw new Error('This action type is undefined')
     }
+}
+
+
+export const removeTodolistAC = (id: string) => {
+    return {type: 'REMOVE-TODOLIST' as const, id}
 }
